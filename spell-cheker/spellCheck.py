716dbda9f8126d3bc5e820000e0a1ca862198530
felerminoali <felerminoali@gmail.com>
2021-09-22 15:58:40 +0100

    22-09-2021

--- spell-cheker/spellCheck.py
+++ spell-cheker/spellCheck.py
@@ -19,52 +19,57 @@ print(f"There are {len(words)} total words in the corpus")
 vocabs = set(words)
 print(f"There are {len(vocabs)} unique words in the vocabulary")
 
-word_counts = Counter(words)
-print(word_counts["yesu"])
+for v in sorted(vocabs):
+  f = open("demofile.txt", "a", encoding="utf8")
+  f.write(v+'\n')
+  f.close()
 
-total_word_count = float(sum(word_counts.values()))
-word_probas = {word: word_counts[word] / total_word_count for word in word_counts.keys()}
-print(word_probas["yesu"])
-
-def split(word):
-  return [(word[:i], word[i:]) for i in range(len(word) + 1)]
-
-print(split("trash"))
-
-def delete(word):
-  return [l + r[1:] for l,r in split(word) if r]
-
-print(delete("trash"))
-
-def swap(word):
-  return [l + r[1] + r[0] + r[2:] for l, r in split(word) if len(r)>1]
-
-print(swap("trash"))
-
-def replace(word):
-  letters = string.ascii_lowercase
-  return [l + c + r[1:] for l, r in split(word) if r for c in letters]
-
-print(replace("trash"))
-
-def replace_for_vmw(word):
-  letters = ['a', 'aa', 'c', 'e', 'ee', 'f', 'h', 'i', 'ii', 'k', 'kh', 'l', 'm', 'n', 'ny', 'ng', 'o', 'oo', 'p', 'ph', 'r', 's', 't', 'th', 'tt', 'tth', 'u', 'uu', 'v', 'w', 'x', 'y']
-  return [l + c + r[1:] for l, r in split(word) if r for c in letters]
-
-print(replace_for_vmw("trash"))
-
-def insert(word):
-  letters = string.ascii_lowercase
-  return [l + c + r for l, r in split(word) for c in letters]
-
-print(insert("trash"))
-
-def insert_for_vmw(word):
-  letters = ['a', 'aa', 'c', 'e', 'ee', 'f', 'h', 'i', 'ii', 'k', 'kh', 'l', 'm', 'n', 'ny', 'ng', 'o', 'oo', 'p', 'ph',
-             'r', 's', 't', 'th', 'tt', 'tth', 'u', 'uu', 'v', 'w', 'x', 'y']
-  return [l + c + r for l, r in split(word) for c in letters]
-
-print(insert_for_vmw("trash"))
-
-def edit1(word):
-  return set(delete(word) + swap(word) + replace_for_vmw(word) + insert_for_vmw(word))
+# word_counts = Counter(words)
+# print(word_counts["yesu"])
+#
+# total_word_count = float(sum(word_counts.values()))
+# word_probas = {word: word_counts[word] / total_word_count for word in word_counts.keys()}
+# print(word_probas["yesu"])
+#
+# def split(word):
+#   return [(word[:i], word[i:]) for i in range(len(word) + 1)]
+#
+# print(split("trash"))
+#
+# def delete(word):
+#   return [l + r[1:] for l,r in split(word) if r]
+#
+# print(delete("trash"))
+#
+# def swap(word):
+#   return [l + r[1] + r[0] + r[2:] for l, r in split(word) if len(r)>1]
+#
+# print(swap("trash"))
+#
+# def replace(word):
+#   letters = string.ascii_lowercase
+#   return [l + c + r[1:] for l, r in split(word) if r for c in letters]
+#
+# print(replace("trash"))
+#
+# def replace_for_vmw(word):
+#   letters = ['a', 'aa', 'c', 'e', 'ee', 'f', 'h', 'i', 'ii', 'k', 'kh', 'l', 'm', 'n', 'ny', 'ng', 'o', 'oo', 'p', 'ph', 'r', 's', 't', 'th', 'tt', 'tth', 'u', 'uu', 'v', 'w', 'x', 'y']
+#   return [l + c + r[1:] for l, r in split(word) if r for c in letters]
+#
+# print(replace_for_vmw("trash"))
+#
+# def insert(word):
+#   letters = string.ascii_lowercase
+#   return [l + c + r for l, r in split(word) for c in letters]
+#
+# print(insert("trash"))
+#
+# def insert_for_vmw(word):
+#   letters = ['a', 'aa', 'c', 'e', 'ee', 'f', 'h', 'i', 'ii', 'k', 'kh', 'l', 'm', 'n', 'ny', 'ng', 'o', 'oo', 'p', 'ph',
+#              'r', 's', 't', 'th', 'tt', 'tth', 'u', 'uu', 'v', 'w', 'x', 'y']
+#   return [l + c + r for l, r in split(word) for c in letters]
+#
+# print(insert_for_vmw("trash"))
+#
+# def edit1(word):
+#   return set(delete(word) + swap(word) + replace_for_vmw(word) + insert_for_vmw(word))
